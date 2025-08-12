2451fe5525fb700938f22ad92f24a42b67bd3f83
fab3dcreation-hub <fab3dcreation@gmail.com>
2025-08-12 18:22:04 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("btn-devis").addEventListener("click", function() {
-    const productName = this.getAttribute("data-product");
-    document.getElementById("product-name").value = productName;
-    document.getElementById("devis-form").style.display = "block";
-});
